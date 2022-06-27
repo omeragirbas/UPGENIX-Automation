7bef81ebc962a4714565b97a7f36df57a9b7a869
agirb <agirbasomer35@gmail.com>
2022-06-27 23:44:05 +0300

    Initial commit

---
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -0,0 +1,23 @@
+package com.upgenix.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-reports.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
+
+        },
+        features = "src/test/resources/features",
+        glue = "com/upgenix/step_definitions" ,
+        dryRun = false ,
+        tags = ""
+)
+public class CukesRunner  {
+
+}
