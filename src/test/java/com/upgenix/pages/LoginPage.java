072b30d876ceec2edf3eb87bfd29a2ae3e34b10f
agirb <agirbasomer35@gmail.com>
2022-06-29 10:05:17 +0300

    Step 4

--- src/test/java/com/upgenix/pages/LoginPage.java
+++ src/test/java/com/upgenix/pages/LoginPage.java
@@ -1,10 +1,19 @@
 package com.upgenix.pages;
 
 import com.upgenix.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginPage {
     public LoginPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
+    @FindBy(id = "username")
+    public WebElement username;
+    @FindBy(id = "password")
+    public WebElement password;
+    @FindBy(linkText = "Log in")
+    public WebElement loginBtn;
+
 }
