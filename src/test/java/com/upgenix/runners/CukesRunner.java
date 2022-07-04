d0f7fe413db7b0e6dae4f529e3293e81942b018b
agirb <agirbasomer35@gmail.com>
2022-07-04 14:56:11 +0300

    login done

--- src/test/java/com/upgenix/runners/CukesRunner.java
+++ src/test/java/com/upgenix/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/upgenix/step_definitions",
         dryRun = false ,
-        tags = "@UPGNX-335"
+        tags = "@UPGNX-332"
 )
 public class CukesRunner  {
 
